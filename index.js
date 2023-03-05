cd2865a01b0bf787348612a8d135134ebf379e24
unknown <IkeChace@gmail.com>
2023-03-05 01:01:30 -0800

    Just some additions

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-array = [3, 9, 23, 64, 2, 8, 28, 93]
-console.log(ages[0] - ages[ages.length - 1])
+var ages = [3, 9, 23, 64, 2, 8, 28, 93]
+console.log(ages[ages.length - 1] - ages[0])
+
+ages.push(43);
+console.log(ages[ages.length - 2] - ages[ages.length - 1])
+
+sum = 0
+for (var i = 0; i < ages.length; i++){
+    sum += ages[i]
+}
+
+var average = sum / ages.length;
+
+console.log(average);
+
+var names = ["sam", "Tommy", "Tim", "Sally", "Buck", "Bob"]
+var lengthOfNamesArray = []
+
+for (i = 0; i < names.length; i++){
+    var individualName = names[i];
+    var individualNameLength = individualName.length;
+    lengthOfNamesArray.push(individualNameLength);
+}
+console.log(lengthOfNamesArray);
+
+sumOfNameLengths = 0
+
+for (i = 0; i < lengthOfNamesArray.length; i++){
+    sumOfNameLengths += lengthOfNamesArray[i]
+}
+
+var averageNamelengths = sumOfNameLengths / lengthOfNamesArray.length;
+
+console.log(averageNameLengths);
